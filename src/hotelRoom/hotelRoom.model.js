a14a5dc592de8bb8fe22cb5a625200567985a478
jgutierrez <jgutierrez-2020370@kinal.edu.gt>
2025-04-07 20:32:15 -0600

    Estructura basica de habitacion de hotel añadida

---
+++ src/hotelRoom/hotelRoom.model.js
@@ -0,0 +1,31 @@
+import { Schema } from 'mongoose';
+
+const hotelRoomSchema = Schema(
+    {
+        number: {
+            type: String,
+            required: [true, 'Room number is required'],
+        },
+        type: {
+            type: String,
+            required: [true, 'Room type is required'],
+            enum: ['single', 'double', 'triple', 'familiar', 'suite'],
+        },
+        price: {
+            type: Number,
+            required: [true, 'Room price is required'],
+        },
+        description: {
+            type: String,
+            required: [true, 'Room description is required'],
+        },
+        imagen: {
+            type:String
+        },
+        hotel: {
+            type: Schema.Types.ObjectId,
+            ref: 'Hotel',
+            required: [true, 'Hotel ID is required'],
+        },
+    }
+)
