bb88a533b58a930042448bd5fb06cd07ec1aa5ee
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-04-07 20:08:30 -0600

    se agrega validacion para register

--- helpers/validators.js
+++ helpers/validators.js
@@ -2,3 +2,13 @@ import { body } from 'express-validator'
 import { validateErrors} from './validate.errors.js'
 import { exitEmailUser } from './db.validators.js'
 
+export const registerValidator = [
+    body('name', 'Name cannot be empty').notEmpty(),
+    body('surname', 'Name cannot be empty').notEmpty(),
+    body('email', 'Email cannot be empty').notEmpty().isEmail().custom(exitEmailUser),
+    body('username', 'Email cannot be empty').notEmpty(),
+    body('password', 'Password cannot be empty').notEmpty().isStrongPassword().withMessage('Password must be strong').isLength({min:8}),
+    body('phone', 'Phone cannot be empty').notEmpty().isMobilePhone(),
+    body('address', 'Address cannot be empty').notEmpty(),
+    validateErrors       
+]
