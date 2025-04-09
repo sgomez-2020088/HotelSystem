c9aa88dd9ecdc0face7a3ff7d60342f81fa0b7ef
JoseArrecis <jarrecis-2020444@Kinal.edu.gt>
2025-04-08 20:47:07 -0600

    Se agrega el controlador de user

---
+++ src/user/user.controller.js
@@ -0,0 +1,193 @@
+import mongoose from "mongoose"
+import User from "./user.model"
+
+export const getAll = async(req, res)=>{
+    try {
+        const { limit = 20, skip = 0 } = req.body
+        const users = await User.find()
+            .skip(skip)
+            .limit(limit)
+
+        if(users.length === 0) return res.status(404).send({ message: 'Users not found', success: false })
+        return res.send(
+            {
+                success: true,
+                message: 'Users found',
+                users,
+                total: users.length
+            }
+        )
+    }catch(err){
+        console.error(err)
+        return res.status(500).send(
+            {
+                success: false,
+                message: 'General error', 
+                err
+            }
+        )
+    }
+}
+
+export const updateUser = async(req, res) =>{
+    try {
+        const { id } = req.params
+        if(!mongoose.Types.ObjectId.isValid(id)){
+            return res.status(400).send(
+                {
+                    success: false,
+                    message: 'Invalida user ID'
+                }
+            )
+        }
+
+        const { name, surname, username, email, phone, role } = req.body
+        const requestingUser = req.user
+        const userToUpdate = await User.findById(id)
+
+        if(!userToUpdate){
+            return res.status(404).send(
+                {
+                    success: false,
+                    message: 'User not found'
+                }
+            )
+        }
+
+        if(userToUpdate.role === 'ADMIN' && role === 'CLIENT') {
+            return res.status(403).send(
+                {
+                    success: false,
+                    message: 'You cannot downgade an ADMIN to CLIENT'
+                }
+            )
+        }
+
+        const updateUser = await User.findByIdAndUpdate(
+            id,
+            { name, surname, username, email, phone, role },
+            { new: true }
+        )
+
+        return res.send(
+            {
+                success: true,
+                message: 'User updated',
+                updateUser
+            }
+        )
+
+    } catch(err){
+        console.error(err)
+        return res.status(500).send(
+            {
+                success: false,
+                message: 'General error', 
+                err
+            }
+        )
+    }
+}
+
+export const updatePassword = async(req, res) => {
+    try {
+        const { id } = req.params
+        const { currentPasswod, newPassword } = req.body
+
+        if(!id || !currentPasswod || !newPassword){
+            return res.status(400).send(
+                {
+                    success: false,
+                    message: 'All fields are required: id, currentPassword, newPassword'
+                }
+            )
+        }
+
+        const user = await User.findById(id)
+        if (!user){
+            return res.status(404).send(
+                {
+                    success: false,
+                    message: 'User not found'
+                }
+            )
+        }
+
+        const isMatch = await argon2.verify(user.password, currentPasswod)
+        if (!isMatch){
+            return res.status(400).send(
+                {
+                    success: false,
+                    message:  'Current password is incorrect'
+                }
+            )
+        }
+
+        const hashedPassword = await argon2.hash(newPassword)
+
+        user.password = hashedPassword
+        await user.save()
+
+        return res.send(
+            {
+                success: true,
+                message: 'Password updated succesfully'
+            }
+        )
+
+    }catch(err){
+        console.error(err)
+        return res.status(500).send(
+            {
+                success: false,
+                message: 'General error', 
+                err
+            }
+        )
+    }
+}
+
+export const deleteOne = async(req, res) =>{
+    try {
+        const userId = req.params.id
+
+        if(!mongoose.Types.ObjectId.isValid(userId)) {
+            return res.status(400).send(
+                {
+                    success: false,
+                    message: 'Invalid user ID'
+                }
+            )
+        }
+
+        const user = await User.findById(userId)
+        if(!user){
+            return res.status(404).send(
+                {
+                    success: false,
+                    message: 'User not found'
+                }
+            )
+        }
+
+        user.status = false
+        await user.save()
+
+        return res.send(
+            {
+                success: true,
+                message: 'User desactived'
+            }
+        )
+            
+    } catch (err) {
+        console.error(err)
+        return res.status(500).send(
+            {
+                success: false,
+                message: "General error",
+                err
+            }
+        )
+    }
+}
