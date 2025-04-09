fb8523f922803fdefaa7efe965734167dc7b8f33
jsican-2023091 <jsican-2023091@kinal.edu.gt>
2025-04-08 20:18:45 -0600

    Se agrega el controlador de events

---
+++ src/events/events.controller.js
@@ -0,0 +1,26 @@
+import eventsModel from './events.model.js'
+import Events from './events.model.js'
+
+export const saveEvent = async(req, res) => {
+    try {
+        let data = req.body
+        let event = new Events(data)
+
+        await event.save()
+        return res.send(
+            {
+                succes: true,
+                message: `Succesfully the event: ${event.name}` 
+            }
+        )
+    } catch (err) {
+        console.error(err)
+        return res.status(500).send(
+            {
+                succes: false,
+                message: 'General error with saving events'
+            }
+        )
+    }
+}
+
