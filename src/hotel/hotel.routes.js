5a76dc71570844dde2aca1c2a0f6c7a3c69f3af7
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-04-08 20:36:36 -0600

    se agregan rutas para clase hotel

---
+++ src/hotel/hotel.routes.js
@@ -0,0 +1,14 @@
+import { Router } from "express"
+import { addHotel, getAllHotels, getHotelById, updateHotel, deleteHotel } from "./hotel.controller"
+import { validateJwt} from "../middlewares/validate.jwt.js"
+
+const api = Router()
+
+api.post('/add',[validateJwt],addHotel)
+api.get('/getAll',[validateJwt], getAllHotels)
+api.get('/getOne',[validateJwt], getHotelById) 
+api.put('/update',[validateJwt], updateHotel)
+api.delete('/delete',[validateJwt], deleteHotel)
+
+export default api
+
