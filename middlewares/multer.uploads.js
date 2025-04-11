fd58b39a652d86a47e629201aee0cc0979b748f5
hcordero-2023253 <hcordero-2023253@kinal.edu.gt>
2025-04-11 07:37:44 -0600

    se crea codigo multer para imagenes

---
+++ middlewares/multer.uploads.js
@@ -0,0 +1,37 @@
+import multer from 'multer'
+import { dirname, extname, join } from 'path'
+import { fileURLToPath } from 'url'
+
+const CURRENT_DIR = dirname(fileURLToPath(import.meta.url)) 
+const MIMETYPES = ['image/jpeg', 'image/png', 'image/jpg', 'image/webp']
+const MAX_SIZE = 10000000
+
+const multerConfig = (destinationPath)=>{
+    return multer(
+        {
+            storage: multer.diskStorage( 
+                {
+                    destination: (req, file, cb)=>{ 
+                        const fullPath = join(CURRENT_DIR, destinationPath)
+                        req.filePath = fullPath
+                        cb(null, fullPath)
+                    },
+                    filename: (req, file, cb)=>{ 
+                        const fileExtension = extname(file.originalname)
+                        const fileName = file.originalname.split(fileExtension)[0]
+                        cb(null, `${fileName}-${Date.now()}${fileExtension}`)
+                    }
+                }
+            ),
+            fileFilter: (req, file, cb)=>{ 
+                if(MIMETYPES.includes(file.mimetype)) cb(null, true)
+                else cb(new Error(`Only ${MIMETYPES.join(' ')} are allowed`))
+            },
+            limits: {
+                fileSize: MAX_SIZE
+            }
+        }
+    )
+}
+
+export const uploadProfilePicture = multerConfig('../uploads/img/hotelMedia') 
