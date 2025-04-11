e491253960938b9574d236812d9eab49aae827d5
hcordero-2023253 <hcordero-2023253@kinal.edu.gt>
2025-04-11 07:38:28 -0600

    se actualizan rutas multer

--- src/auth/auth.routes.js
+++ src/auth/auth.routes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express'
 import { register,login } from './auth.controller.js'
+import { uploadProfilePicture } from '../../middlewares/multer.uploads.js'
 
 const api = Router()
 
 //PUBLIC ROUTES
-api.post('/register', register)
+api.post('/register',[uploadProfilePicture.single('profilePicture')], register)
 
 api.post('/login', login)
 
