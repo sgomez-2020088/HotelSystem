a910c4fbd8be3b8b476b8339220381476512f843
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-04-12 18:30:12 -0600

    Se arregla funcion validateJwt

--- middlewares/validate.jwt.js
+++ middlewares/validate.jwt.js
@@ -1,29 +1,18 @@
 'use strict '
 import jwt from 'jsonwebtoken'
-import { findUser } from '../helpers/db.validators.js'
 
-export const validateJwt = async(req, res, next)=>{
-    try{
+
+export const validateJwt = async(req, res, next) =>{
+    try {
         let secretKey = process.env.SECRET_KEY
         let { authorization } = req.headers
-        if(!authorization) return res.status(401).send({message: 'Unauthorized'})
-        let user = jwt.verify(authorization, secretKey)
-        const validateUser = await findUser(user.uid)
-        if(!validateUser) return res.status(404).send(
-            {
-                success: false,
-                message: 'User not found - Unauthorized'
-            }
-        )
-        req.user = {
-            id: validateUser._id,
-            email: validateUser.email,
-            role: validateUser.role
-        }
+        if(!authorization) return res.status(401).send({success: false, message: 'You must be logged in'})
+        let user = jwt.verify(authorization,secretKey)
+        req.user = user
         next()
-    }catch(err){
+        } catch (err) {
         console.error(err)
-        return res.status(401).send({message: 'Invalid token or expired'})
+        return res.status(401).send({success: false, message: 'Invalid Token'})
     }
 }
 
