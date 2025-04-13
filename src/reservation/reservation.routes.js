a59300f2dfbe51efc8592fe10474d8ed3d26b310
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-04-12 20:18:22 -0600

    Se agrega nueva ruta de Reservation

--- src/reservation/reservation.routes.js
+++ src/reservation/reservation.routes.js
@@ -1,10 +1,11 @@
 
 import { Router } from "express";
-import { addReservation } from "./reservation.controller.js";
+import { addReservation,getReservationsHotel } from "./reservation.controller.js";
 import { validateJwt } from "../../middlewares/validate.jwt.js";
 
 const api = Router()
 
 api.post('/reservate',[validateJwt], addReservation)
+api.get('/reservationsHotel',[validateJwt], getReservationsHotel)
 
 export default api
