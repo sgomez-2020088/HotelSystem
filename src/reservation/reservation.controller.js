bfe52ed51867f3cafda75731a7100449102cb3d8
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-04-12 20:17:56 -0600

    Se agrega nuevas funciones para Reservation

--- src/reservation/reservation.controller.js
+++ src/reservation/reservation.controller.js
@@ -1,20 +1,23 @@
-import User from "../user/user.model.js";
-import Reservation from "./reservation.model.js";
-import Hotel from "../hotel/hotel.model.js";
-import HotelRoom from "../hotel/hotel.model.js";
+import User from "../user/user.model.js"
+import Reservation from "./reservation.model.js"
+import Hotel from "../hotel/hotel.model.js"
+import HotelRoom from "../hotelRoom/hotelRoom.model.js"
 
 export const addReservation = async (req, res) => {
     try {
         let data = req.body
 
         data.user = req.user.uid
+
+        let checkIn = new Date(data.checkIn).getUTCDate()
+        let checkOut = new Date(data.checkOut).getUTCDate()
         
-        console.log(data.user)
-        
-        let hotelId = await Hotel.findById(data.hotelId)
+        let hotelId = await Hotel.findById(data.hotel)
         if(!hotelId) return res.status(404).send({ message: 'Hotel not found', success: false })
             
-        let hotelRoomId = await HotelRoom.findById(data.hotelRoomId)
+        let hotelRoomId = await HotelRoom.findById(data.hotelRoom)
+        let days = checkOut - checkIn
+        data.total = (hotelRoomId.price * days)
         if(!hotelRoomId) return res.status(404).send({ message: 'Hotel room not found', success: false })    
         
         let reservation = new Reservation(data)
@@ -26,3 +29,23 @@ export const addReservation = async (req, res) => {
         return res.status(500).send({success:false, message:'General Error',err})
     }
 }
+
+export const getReservationsHotel = async (req, res) => {
+    try {
+        let {hotelName} = req.body
+
+        let hotel = await Hotel.findOne({name: hotelName})
+        if(!hotel) return res.status(404).send({ message: 'Hotel not found', success: false })
+        
+        let reservations = await Reservation.find({hotel: hotelName})
+        if(!reservations) return res.status(404).send({ message: 'Reservations not found', success: false })
+            
+        return res.send({success: true, message:'Reservations found', reservations})
+    } catch (err) {
+        console.error(err)
+        return res.status(500).send({success:false, message:'General Error',err})
+    }
+}
+
+//Get My Reservations
+
