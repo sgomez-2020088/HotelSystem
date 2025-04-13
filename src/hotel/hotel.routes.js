78c0f27ccc2d35b6d5c6e815806f0d3d4b651337
jgutierrez <jgutierrez-2020370@kinal.edu.gt>
2025-04-12 22:02:56 -0600

    Validacion añadida a las rutas

--- src/hotel/hotel.routes.js
+++ src/hotel/hotel.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express"
 import { addHotel, getAllHotels, getHotelById, updateHotel, deleteHotel } from "./hotel.controller.js"
 import { validateJwt} from '../../middlewares/validate.jwt.js'
+import { hotelValidator } from "../../helpers/validators.js"
 
 const api = Router()
 
-api.post('/add',[validateJwt],addHotel)
+api.post('/add',[validateJwt, hotelValidator], addHotel)
 api.get('/getAll',[validateJwt], getAllHotels)
 api.get('/getOne',[validateJwt], getHotelById) 
 api.put('/update',[validateJwt], updateHotel)
