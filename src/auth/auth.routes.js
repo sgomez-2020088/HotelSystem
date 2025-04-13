1bb9ad987c05d2ad4aa7154f8c5abf4f3431001d
jgutierrez <jgutierrez-2020370@kinal.edu.gt>
2025-04-12 21:39:24 -0600

    Validación para el login añadida

--- src/auth/auth.routes.js
+++ src/auth/auth.routes.js
@@ -1,12 +1,13 @@
 import { Router } from 'express'
 import { register,login } from './auth.controller.js'
 import { uploadProfilePicture } from '../../middlewares/multer.uploads.js'
+import { loginValidator } from '../../helpers/validators.js'
 
 const api = Router()
 
 //PUBLIC ROUTES
 api.post('/register',[uploadProfilePicture.single('profilePicture')], register)
 
-api.post('/login', login)
+api.post('/login', [loginValidator], login)
 
 export default api
