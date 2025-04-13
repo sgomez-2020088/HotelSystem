74376ef698c81369f66c089cfae9e5d705097e0d
jgutierrez <jgutierrez-2020370@kinal.edu.gt>
2025-04-12 21:19:51 -0600

    Rutas para actualizar y eliminar añadidas

--- src/hotelRoom/hotelRoom.routes.js
+++ src/hotelRoom/hotelRoom.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express"
 import { validateJwt } from "../../middlewares/validate.jwt.js"
-import { addHotelRoom, getAllHotelRoom, getHotelById } from "./hotelRoom.controller.js"
+import { addHotelRoom, deleteHotelRoom, getAllHotelRoom, getHotelById, updateHotelRoom } from "./hotelRoom.controller.js"
 
 const api = Router()
 
 api.post('/add',[validateJwt],addHotelRoom)
 api.get('/getAll',[validateJwt], getAllHotelRoom)
 api.get('/getOne',[validateJwt], getHotelById)
+api.put('/update',[validateJwt], updateHotelRoom)
+api.delete('/delete',[validateJwt], deleteHotelRoom)
 
 export default api
