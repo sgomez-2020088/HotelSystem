7842399fca51da8002cb9060283113134a4bccf0
jsican-2023091 <jsican-2023091@kinal.edu.gt>
2025-04-26 17:35:56 -0600

    Se agregan rutas

--- src/events/events.routes.js
+++ src/events/events.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express"
-import { saveEvent } from "./events.controller.js"
+import { deleteEvent, getAllEvents, getEventById, saveEvent, updateEvent } from "./events.controller.js"
 
 const api = Router()
-api.post(
-    '/save',
-    saveEvent
-)
+api.post('/save',saveEvent)
+api.get('/getAll', getAllEvents)
+api.get('/getOne/:id', getEventById)
+api.put('/update/:id', updateEvent)
+api.delete('/delete/:id', deleteEvent)
 
 export default api
