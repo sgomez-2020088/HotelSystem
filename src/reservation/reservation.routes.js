90417060c668f0d4f590d304cd41c4031516ffd7
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-05-01 22:28:47 -0600

    Se agregan las nuevas rutas a las rutas

--- src/reservation/reservation.routes.js
+++ src/reservation/reservation.routes.js
@@ -2,14 +2,15 @@
 import { Router } from "express";
 import { addReservation,cancelReservation,getMyHistoryReservations,getMyReservations,getReservationsHotel } from "./reservation.controller.js";
 import { validateJwt } from "../../middlewares/validate.jwt.js";
+import { cancelReservationValidator, reservationHotelValidator, reservationValidator } from "../../helpers/validators.js";
 
 
 const api = Router()
 
-api.post('/reservate',[validateJwt], addReservation)
-api.get('/reservationsHotel',[validateJwt], getReservationsHotel)
+api.post('/reservate',[validateJwt,reservationValidator], addReservation)
+api.get('/reservationsHotel',[validateJwt, reservationHotelValidator], getReservationsHotel)
 api.get('/myReservations',[validateJwt],getMyReservations)
 api.get('/myHistoryReservations',[validateJwt],getMyHistoryReservations)
-api.delete('/deleteReservation',[validateJwt], cancelReservation)
+api.delete('/deleteReservation',[validateJwt, cancelReservationValidator], cancelReservation)
 
 export default api
