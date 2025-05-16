29bb5da3cf80889c7adb2e75502968baa9816be9
jsican-2023091 <jsican-2023091@kinal.edu.gt>
2025-05-16 17:57:24 -0600

    Se agregan las validaciones a las rutas de eventos

--- src/events/events.routes.js
+++ src/events/events.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express"
 import { deleteEvent, getAllEvents, getEventById, saveEvent, updateEvent } from "./events.controller.js"
+import { eventsValidator } from "../../helpers/validators.js"
 
 const api = Router()
-api.post('/save',saveEvent)
+api.post('/save',[eventsValidator],saveEvent)
 api.get('/getAll', getAllEvents)
 api.get('/getOne/:id', getEventById)
 api.put('/update/:id', updateEvent)
