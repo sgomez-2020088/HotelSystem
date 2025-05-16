98bdfe92cdf2748b4970a9a93cbb1a47a03d4864
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-05-15 21:35:50 -0600

    se actualizan rutas

--- src/user/user.routes.js
+++ src/user/user.routes.js
@@ -1,13 +1,15 @@
 import { Router } from "express";
-import { deleteOne, getAll, getOne, updatePassword, updateUser } from "./user.controller.js";
-import { validateJwt } from '../../middlewares/validate.jwt.js'
- 
+import { deleteOne, getAll, getOne, updatePassword, updateUser,updateUserRole } from "./user.controller.js";
+import { isAdmin, validateJwt } from '../../middlewares/validate.jwt.js'
+import { getOneUserValidator , updateUserRoleValidator,updateUserValidator, updateUserPassValidator} from "../../helpers/validators.js";
+
 const api = Router()
- 
-api.get('/getAll',[validateJwt], getAll)
-api.get('/getOne',[validateJwt],getOne)
-api.put('/updateUser',[validateJwt],updateUser)
-api.put('/updatePassword',[validateJwt],updatePassword)
+
+api.get('/getAll',[validateJwt, isAdmin], getAll)
+api.get('/getOne',[validateJwt,isAdmin, getOneUserValidator],getOne)
+api.put('/updateUser',[validateJwt,updateUserValidator],updateUser)
+api.put('/updatePassword',[validateJwt,updateUserPassValidator],updatePassword)
+api.put('/updateRole',[validateJwt, isAdmin, updateUserRoleValidator],updateUserRole)
 api.delete('/deleteOne',[validateJwt],deleteOne)
- 
+
 export default api
