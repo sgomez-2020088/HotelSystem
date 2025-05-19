28aa79deaf5930f28cd6502ca1ac5a7d9d59a732
jgutierrez-2020370 <jgutierrez-2020370@kinal.edu.gt>
2025-05-19 10:40:23 -0600

    Metodos para obtener actualizados y solución a un problema en agregar

--- src/hotelRoom/hotelRoom.routes.js
+++ src/hotelRoom/hotelRoom.routes.js
@@ -1,13 +1,15 @@
 import { Router } from "express"
 import { validateJwt } from "../../middlewares/validate.jwt.js"
-import { addHotelRoom, deleteHotelRoom, getAllHotelRoom, getHotelById, updateHotelRoom } from "./hotelRoom.controller.js"
+import { addHotelRoom, deleteHotelRoom, getOneRoom, getRoomsFromHotel, updateHotelRoom } from "./hotelRoom.controller.js"
 
 const api = Router()
 
 api.post('/add',[validateJwt],addHotelRoom)
-api.get('/getAll',[validateJwt], getAllHotelRoom)
-api.delete('/delete/:id',[validateJwt], deleteHotelRoom)
 api.put('/update/:id',[validateJwt], updateHotelRoom)
-api.get('/getOne/:id',[validateJwt], getHotelById)
+api.delete('/delete/:id',[validateJwt], deleteHotelRoom)
+api.get('/find/:id', [validateJwt], getOneRoom)
+api.get('/:id/rooms', [validateJwt], getRoomsFromHotel)
+
+
 
 export default api
