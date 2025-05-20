48f491749e58b3f9a67c01c786afc8af618e858e
Sergio Gomez <sergiogomez@192.168.1.9>
2025-05-19 22:53:58 -0600

    se actualizan rutas

--- src/auth/auth.routes.js
+++ src/auth/auth.routes.js
@@ -1,12 +1,11 @@
 import { Router } from 'express'
 import { register,login } from './auth.controller.js'
-import { uploadProfilePicture } from '../../middlewares/multer.uploads.js'
 import { loginValidator, registerValidator } from '../../helpers/validators.js'
 
 const api = Router()
 
 //PUBLIC ROUTES
-api.post('/register',[registerValidator,uploadProfilePicture.single('profilePicture')], register)
+api.post('/register',[registerValidator], register)
 
 api.post('/login', [loginValidator], login)
 
