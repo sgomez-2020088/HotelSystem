2a9ff01c857e2c2ace764e6290ed76db4a1147d4
Sergio Gomez <sergiogomez@192.168.1.9>
2025-05-19 22:54:48 -0600

    se actualizan rutas

--- src/hotel/hotel.routes.js
+++ src/hotel/hotel.routes.js
@@ -2,10 +2,22 @@ import { Router } from "express"
 import { addHotel, getAllHotels, getHotelById, updateHotel, deleteHotel } from "./hotel.controller.js"
 import { validateJwt} from '../../middlewares/validate.jwt.js'
 import { hotelValidator } from "../../helpers/validators.js"
+import { deleteFileOnError } from "../../middlewares/delete.file.on.error.js"
+import { uploadHotelPicture } from '../../middlewares/multer.uploads.js'
 
 const api = Router()
 
-api.post('/add',[validateJwt, hotelValidator], addHotel)
+api.post(
+    '/add',
+    [
+        validateJwt,
+        uploadHotelPicture.single('hotelImages'),
+        hotelValidator,
+        deleteFileOnError
+    ],
+    addHotel
+)
+
 api.get('/getAll',[validateJwt], getAllHotels)
 api.get('/getOne',[validateJwt], getHotelById) 
 api.put('/update',[validateJwt], updateHotel)
