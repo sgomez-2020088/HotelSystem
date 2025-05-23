7b77ac2e0142383a1312f8f6d4f89947cbc45ddb
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-05-22 23:43:02 -0600

    se actualizan rutas

--- src/hotel/hotel.routes.js
+++ src/hotel/hotel.routes.js
@@ -1,9 +1,8 @@
 import { Router } from "express"
 import { addHotel, getAllHotels, getHotelById, updateHotel, deleteHotel } from "./hotel.controller.js"
-import { validateJwt} from '../../middlewares/validate.jwt.js'
-import { hotelValidator } from "../../helpers/validators.js"
-import { deleteFileOnError } from "../../middlewares/delete.file.on.error.js"
-import { uploadHotelPicture } from '../../middlewares/multer.uploads.js'
+import { validateJwt, isAdmin} from '../../middlewares/validate.jwt.js'
+import { addHotelValidator,updateHotelValidator } from "../../helpers/validators.js"
+
 
 const api = Router()
 
@@ -11,17 +10,16 @@ api.post(
     '/add',
     [
         validateJwt,
-        uploadHotelPicture.single('hotelImages'),
-        hotelValidator,
-        deleteFileOnError
+        isAdmin,
+        addHotelValidator
     ],
     addHotel
 )
 
-api.get('/getAll',[validateJwt], getAllHotels)
+api.get('/getAll',[validateJwt,], getAllHotels)
 api.get('/getOne/:id',[validateJwt], getHotelById) 
-api.put('/update',[validateJwt], updateHotel)
-api.delete('/delete/:id',[validateJwt], deleteHotel)
+api.put('/update',[validateJwt, isAdmin,updateHotelValidator], updateHotel)
+api.delete('/delete/:id',[validateJwt, isAdmin], deleteHotel)
 
 export default api
 
