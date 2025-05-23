c34db6bee49509cf63efd461d12fe2f6e15a7625
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-05-22 23:44:25 -0600

    se actualizan rutas

--- src/hotelRoom/hotelRoom.routes.js
+++ src/hotelRoom/hotelRoom.routes.js
@@ -1,13 +1,13 @@
 import { Router } from "express"
-import { validateJwt } from "../../middlewares/validate.jwt.js"
+import { validateJwt, isAdmin } from "../../middlewares/validate.jwt.js"
 import { addHotelRoom, deleteHotelRoom, getOneRoom, getRoomsFromHotel, updateHotelRoom } from "./hotelRoom.controller.js"
-import { hotelRoomValidator } from "../../helpers/validators.js"
+import { addHotelRoomValidator, updateHotelRoomValidator } from "../../helpers/validators.js"
 
 const api = Router()
 
-api.post('/add',[validateJwt],addHotelRoom)
-api.put('/update/:id',[validateJwt], updateHotelRoom)
-api.delete('/delete/:id',[validateJwt], deleteHotelRoom)
+api.post('/add',[validateJwt,isAdmin],addHotelRoom)
+api.put('/update/:id',[validateJwt,isAdmin], updateHotelRoom)
+api.delete('/delete/:id',[validateJwt,isAdmin], deleteHotelRoom)
 api.get('/find/:id', getOneRoom)
 api.get('/:id/rooms' , getRoomsFromHotel)
 
