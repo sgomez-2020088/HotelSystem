42ae177165f79f1b663c614985854efc1d0e65b9
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-05-23 18:12:00 -0600

    se agrega get de usuario

--- src/bill/bill.controller.js
+++ src/bill/bill.controller.js
@@ -48,3 +48,31 @@ export const getAllBill = async (req, res)=>{
         return res.status(500).send({message: 'General error getting all bills', success: false})
     }
 }
+
+export const getMyBills = async (req, res) => {
+  try {
+    const userId = req.user.uid
+
+    const bills = await Bill.find()
+      .populate({
+        path: 'reservation',
+        match: { user: userId },
+        populate: [
+          { path: 'user', select: 'name email -_id' },
+          { path: 'hotel', select: 'name location -_id' },
+          { path: 'hotelRoom', select: 'number type price -_id' }
+        ]
+      })
+
+    const userBills = bills.filter(b => b.reservation !== null)
+
+    if (userBills.length === 0) {
+      return res.status(404).send({ message: 'No bills found for this user', success: false })
+    }
+
+    return res.send({ message: 'Bills found for user', success: true, bills: userBills })
+  } catch (err) {
+    console.error(err)
+    return res.status(500).send({ message: 'General error getting user bills', success: false })
+  }
+}
