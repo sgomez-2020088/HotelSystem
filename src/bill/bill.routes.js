124d6011c176e48c75d2385a793656a0c2023e7a
sgomez-2020088 <sgomez-2020088@kinal.edu.gt>
2025-05-23 18:12:16 -0600

    se actulizan rutas:

--- src/bill/bill.routes.js
+++ src/bill/bill.routes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express'
-import { addBill, getAllBill } from './bill.controller.js'
+import { addBill, getAllBill,getMyBills } from './bill.controller.js'
 import { validateJwt } from '../../middlewares/validate.jwt.js'
 
 
 const api = Router()
 api.post('/add',[validateJwt], addBill)
 api.get('/getAll',[validateJwt], getAllBill)
+api.get('/getMyBills',[validateJwt], getAllBill)
 
 export default api
